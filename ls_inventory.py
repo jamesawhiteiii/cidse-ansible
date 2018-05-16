79f8ff5223f8bd0ffb02127a3886cdb82efebca9
Jon Anderson <jonanderson10@gmail.com>
2018-05-16 14:27:30 -0700

    Initial commit of common role Started (barely) the inventory script for the Landscape API

---
+++ ls_inventory.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+############################################################################################
+# CIDSE Landscape & Ansible Inventory Python Script                                        #
+############################################################################################
+'''
+DESCRIPTION
+This script is designed to interface with Canonical's Landscape API and retrieve a list of
+managed computers for import into inventory files for Ansible
+
+DEPENDENCIES
+	- landscape-api package installed on your system (via apt or apt-get)
+	
+USAGE
+
+
+'''
